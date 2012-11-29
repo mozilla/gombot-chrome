5da5673e0fbbbe975fda557b2faf54c9e0a60599
Zachary Carter <zack.carter@gmail.com>
2012-11-29 15:52:45 -0800

    added sign in page

--- pages/common/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-
-  $('#pin-info-link').click(function (ev) {
-    $('#account-form').toggleClass('show-info');
-  });
-
-});
--- pages/first_run/create_account.js
+++ pages/first_run/create_account.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
+
+    $('#pin-info-link').click(function (ev) {
+        $('#account-form').toggleClass('show-info');
+    });
+
     $('#account-form').submit(function() {
         // Validate form
         if ($('[name="pin"]').get()[0].value != $('[name="pin-repeat"]').get()[0].value) {
             alert('PINs must match!');
             return false;
         }
-    	var backgroundPage = chrome.extension.getBackgroundPage();
+        var backgroundPage = chrome.extension.getBackgroundPage();
         // Set user PIN
         backgroundPage.setAndSavePIN($('[name="pin"]').get()[0].value);
         backgroundPage.firstRunFinished();
