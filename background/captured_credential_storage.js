e7f55f93d13502a265452ed39576fdaca66752c5
Chris Karlof <ckarlof@mozilla.com>
2013-01-23 15:21:08 -0800

    more tabs -> spaces

--- background/captured_credential_storage.js
+++ background/captured_credential_storage.js
@@ -1,62 +1,62 @@
 var CapturedCredentialStorage = function(Gombot, Uri) {
 
-	var storage = {};
+  var storage = {};
 
-	function mergeCredentials(newCredentials, source) {
-		var oldCredentials = storage[source.id];
-		// TODO: revisit strict equality requirement here
-		if (!oldCredentials || newCredentials.origin !== oldCredentials.origin) {
-			storage[source.id] = newCredentials;
-			console.log("Storing new credentials", storage[source.id], Gombot.getCurrentUser());
-			return;
-		}
-		if (newCredentials.password) {
-			oldCredentials.password = newCredentials.password;
-		}
-		if (newCredentials.username) {
-			oldCredentials.username = newCredentials.username;
-		}
-		console.log("Merging credentials", storage[source.id], Gombot.getCurrentUser());
-	}
+  function mergeCredentials(newCredentials, source) {
+    var oldCredentials = storage[source.id];
+    // TODO: revisit strict equality requirement here
+    if (!oldCredentials || newCredentials.origin !== oldCredentials.origin) {
+      storage[source.id] = newCredentials;
+      console.log("Storing new credentials", storage[source.id], Gombot.getCurrentUser());
+      return;
+    }
+    if (newCredentials.password) {
+      oldCredentials.password = newCredentials.password;
+    }
+    if (newCredentials.username) {
+      oldCredentials.username = newCredentials.username;
+    }
+    console.log("Merging credentials", storage[source.id], Gombot.getCurrentUser());
+  }
 
-	function cleanupLoginUrl(url) {
-		var u = new Uri(url);
-		u.anchor("");
-		u.query("");
-		u.userInfo("");
-		return u.toString();
-	}
+  function cleanupLoginUrl(url) {
+    var u = new Uri(url);
+    u.anchor("");
+    u.query("");
+    u.userInfo("");
+    return u.toString();
+  }
 
-	// Stores captured credentials
-	// Input:
-	//   credentials: object with properties:
-	//     usernames: a username
-	//     password: a password
-	//     id: identifier for the credentials
-	//   source: object with properties:
-	//     id: identifier for the credential's source
-	//     url: url of the credential's source
-	function setCredentials(credentials, source) {
-		credentials.origin = Gombot.Realms.getOriginForUri(source.url);
-		credentials.loginurl = cleanupLoginUrl(source.url);
-		mergeCredentials(credentials, source);
-	}
+  // Stores captured credentials
+  // Input:
+  //   credentials: object with properties:
+  //     usernames: a username
+  //     password: a password
+  //     id: identifier for the credentials
+  //   source: object with properties:
+  //     id: identifier for the credential's source
+  //     url: url of the credential's source
+  function setCredentials(credentials, source) {
+    credentials.origin = Gombot.Realms.getOriginForUri(source.url);
+    credentials.loginurl = cleanupLoginUrl(source.url);
+    mergeCredentials(credentials, source);
+  }
 
-	function getCrendentials(credentials, source, callback) {
-		callback(storage[source.id]);
-		console.log("Getting credentials", storage[source.id]);
-	}
+  function getCrendentials(credentials, source, callback) {
+    callback(storage[source.id]);
+    console.log("Getting credentials", storage[source.id]);
+  }
 
-	function deleteCredentials(source) {
-		console.log("Deleting credentials for tab", source.id)
-		delete storage[source.id]
-	}
+  function deleteCredentials(source) {
+    console.log("Deleting credentials for tab", source.id)
+    delete storage[source.id]
+  }
 
-	return {
-		setCredentials: setCredentials,
-		getCredentials: getCrendentials,
-		deleteCredentials: deleteCredentials
-	};
+  return {
+    setCredentials: setCredentials,
+    getCredentials: getCrendentials,
+    deleteCredentials: deleteCredentials
+  };
 };
 
 if (typeof module !== "undefined" && module.exports) {
--- background/collections/user_collection.js
+++ background/collections/user_collection.js
@@ -1,9 +1,13 @@
 var UserCollection = function(Backbone, _, User, LocalStorage) {
 
-	var UserCollection = Backbone.Collection.extend({
-		model: User,
-		localStorage: LocalStorage
-	});
+  var UserCollection = Backbone.Collection.extend({
+    model: User,
+    localStorage: LocalStorage
+  });
 
-	return UserCollection;
+  return UserCollection;
 };
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = UserCollection;
+}
